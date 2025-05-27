b1dcaa6cbda6e5fdf954fc65eeba00603db52fe3
Jagdish <jagdishgaikwad8765@gmail.com>
2025-05-27 16:46:45 +0530

    Lecture 1

---
+++ Lecture1/data.py
@@ -0,0 +1,6 @@
+name = 'jagdish'
+age = 20
+TorF= False
+a= None
+
+print(type(age), type(name), type(TorF), type(a))
---
+++ Lecture1/variables.py
@@ -0,0 +1,8 @@
+#all info about iphone 15 pro max
+Device_info="Iphone 15 Pro Max"
+Weight=215
+Price= 850000
+
+print("The Device Name is :", Device_info)
+print("The weight of Device in Grams :", Weight)
+print("The Price of Device In Rupees :" , Price)
