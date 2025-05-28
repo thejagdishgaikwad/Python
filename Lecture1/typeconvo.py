d8106053312c852120add4abb81a36623aaa942c
Jagdish <jagdishgaikwad8765@gmail.com>
2025-05-28 14:36:39 +0530

    Type Casting and Type Conversion Topic Programs

---
+++ Lecture1/typeconvo.py
@@ -0,0 +1,26 @@
+#Type Conversion (Implicit Conversion)
+a=13.09
+b=22
+
+c=a+b
+print("The Implicit Convo :", c)
+
+
+
+#Type Casting (Explicit Conversion)
+a=5.12
+b= "  Jagdish"
+c = str(a)
+
+final = c + b
+print("The Explicit Convo :", final)
+print(type(final))
+print(type(c))
+
+no1=6.32
+no2=4
+No=int(no1)
+
+sum= No+no2
+print(type(sum))
+print("The Explicit Convo : ", sum)
