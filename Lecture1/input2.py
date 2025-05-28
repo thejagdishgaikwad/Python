464b5dd44f85d9db7d7a4d74469df1bb74a30d96
Jagdish <jagdishgaikwad8765@gmail.com>
2025-05-28 14:35:59 +0530

    Input topic programs

---
+++ Lecture1/input2.py
@@ -0,0 +1,5 @@
+name = input("Enter Your Name : ")
+age = int(input("Enter Your Age : "))
+marks = int(input("Enter Your Academic Marks 100/ :"))
+
+print("Hello My Name Is,", name, "My age is", age, "I have scored", marks,"marks in my Whole academics!")
---
+++ Lecture1/inputs.py
@@ -0,0 +1,14 @@
+name = input("Enter Your Name : ")
+age = int(input("Enter Your Age : "))
+your_no = int(input("Enter Your Contact NO : "))
+
+no= 9653454600
+Name= "Jagdish" or "jagdish" #test of upper and lower case
+Age= 20
+
+if(Name == name) and (age == Age) and (no == your_no):
+    print("The Correct Guest Has Arrived!\n")
+elif(name == Name) or (no == your_no): 
+    print("Maybe Right Guest!")
+else:
+    print("NOBODY IS CORRECT!")
