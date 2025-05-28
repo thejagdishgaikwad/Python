1233289b5c83982e36c753114f0711843421288f
Jagdish <jagdishgaikwad8765@gmail.com>
2025-05-28 18:51:14 +0530

    Slicing topic program

---
+++ Lecture2/slice.py
@@ -0,0 +1,6 @@
+Name = input("Enter Your Name :")
+print("My Name is :", Name)
+Start_idx = int(input("Enter Start Index :"))
+Ending_idx = int(input("Enter Ending Index, By Doing N-1 :"))
+
+print("String After slicing :", Name[Start_idx:Ending_idx])
