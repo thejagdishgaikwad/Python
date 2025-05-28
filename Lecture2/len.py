1cbdcc42fc2989c119b289dff58cbfed4e69d663
Jagdish <jagdishgaikwad8765@gmail.com>
2025-05-28 17:43:16 +0530

    Length function program

---
+++ Lecture2/len.py
@@ -0,0 +1,3 @@
+String = "Once upon a time in a quiet village, there lived a curious boy named Aryan. He loved exploring forests and collecting strange rocks. One day, he found a glowing stone under an old tree. From that moment, magical things began to happen — animals talked, plants danced, and Aryan’s adventures truly began."
+
+print(len(String))
