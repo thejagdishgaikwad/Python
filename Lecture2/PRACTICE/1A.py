31a59f2780059b4a4b3e869501adf145a494d123
Jagdish <jagdishgaikwad8765@gmail.com>
2025-05-29 19:35:01 +0530

    PRACTICE QN's

---
+++ Lecture2/PRACTICE/1A.py
@@ -0,0 +1,3 @@
+#write a python program to get user based string as input and print its length or count its final index
+String = input("Enter any sort of string :")
+print("The length or Nth Index of :", String, ":", len(String))
---
+++ Lecture2/PRACTICE/1B.py
@@ -0,0 +1,4 @@
+#write a python program to count the occurence of $ symbol in the string
+String =  "The treasure chest contained $$$, and the pirate shouted, 'More $$$! I want all the $$$ in the world!' But even with all those $$$, he couldn't buy happiness."
+
+print("The occurence of the $ Symbol in the string :", String.count("$"))
