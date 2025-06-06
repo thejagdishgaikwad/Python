6632b2bb8a13e49e55534ab36f8375994d065f36
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-06 18:05:56 +0530

    Modified program

--- Lecture2/Strings/StringFunctions/endswith.py
+++ Lecture2/Strings/StringFunctions/endswith.py
@@ -1,3 +1,3 @@
 String = input("Enter The String Of Your Choice :")
-print("If the string ends with sh :", String.endswith("sh"))
 print(String)
+print("If the string ends with sh :", String.endswith("sh"))
