abb9705bfda0ac2de5a415d06a6eff245b0269c4
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-06 18:07:16 +0530

    added list1

---
+++ Lecture2/List_Tuples/list1.py
@@ -0,0 +1,6 @@
+Alphabets = ["Jagdish!", "I am", "ROBO 1Thz"]
+idx = int(input("Enter the index no :"))
+print("The element at index ", idx,":",Alphabets[idx])
+Alphabets[2] = "Duggu"
+print("The Complete String :", Alphabets)
+print("The string after slicing :", Alphabets[0:2])
