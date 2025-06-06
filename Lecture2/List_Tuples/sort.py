85307f5ab4b0e45266527e35c793dad81e1ba735
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-06 14:28:53 +0530

    Updating code with alphabets too

--- Lecture2/List_Tuples/sort.py
+++ Lecture2/List_Tuples/sort.py
@@ -1,5 +1,10 @@
 Numbers = [8,6,3,0,-4,11]
+Names = ["Jagdish", "Raosaheb", "Gaikwad", "Priyanka", "Yosef", "Dethe"]
 Numbers.sort()
+Names.sort()
+print("Names in ascending Order :", Names)
 print("List in Ascending Order :", Numbers)
 Numbers.sort(reverse=True)
+Names.sort(reverse=True)
+print("Names in descending Order :",Names)
 print("List in Descending Order :", Numbers)
