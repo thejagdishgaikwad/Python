f5dbc947d07c179b2c43419eb1b30f151e698463
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-07 19:41:35 +0530

    list remove fn

---
+++ Lecture2/List_Tuples/remove.py
@@ -0,0 +1,4 @@
+alphabets = ["Jagdish", "Raosaheb", "Gaikwad", "Gaikwad", "Jagdish"]
+print("The alphabets before :",alphabets)
+alphabets.remove("Jagdish")
+print("The alphabets after removal :",alphabets)
