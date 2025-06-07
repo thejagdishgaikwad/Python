25d694aa0834170f3d3cd89f5edeb53650d8d5c1
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-07 21:06:34 +0530

    Write a python program to check if a list contains a palindrome of elemebts or not

---
+++ Lecture2/PRACTICE/2B.py
@@ -0,0 +1,14 @@
+Data = []
+Data.append(int(input("Enter the data element 1:")))
+Data.append(int(input("Enter the data element 2:")))
+Data.append(int(input("Enter the data element 3:")))
+
+print(Data)
+Data2 = Data.copy()
+Data2.reverse()
+print(Data2.reverse())
+print(Data == Data2)
+if(Data2 == Data):
+    print("The Given Number Is Palindrome!\n")
+else:
+    print("The Given Number Is Not Palindrome!\n")
