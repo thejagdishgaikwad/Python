f938da47f2e0c23ca9307d4d8c04b26d6fe85242
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-07 20:24:23 +0530

    Modified : Declared append fn before using it

--- Lecture2/List_Tuples/append.py
+++ Lecture2/List_Tuples/append.py
@@ -1,4 +1,4 @@
 Mydata = [4, 6, 9, 2]
 no = int(input("Enter any random no :"))
-Mydata.append(no)
+Mydata.append(no) #the .append function must be declare before using it!
 print("So the updated list :",Mydata)
