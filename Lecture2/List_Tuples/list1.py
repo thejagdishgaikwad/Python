525e253abfffc6be4cee403505ea8b355965d9dc
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-07 20:25:16 +0530

    Modified : Slicing fn

--- Lecture2/List_Tuples/list1.py
+++ Lecture2/List_Tuples/list1.py
@@ -3,4 +3,4 @@ idx = int(input("Enter the index no :"))
 print("The element at index ", idx,":",Alphabets[idx])
 Alphabets[2] = "Duggu"
 print("The Complete String :", Alphabets)
-print("The string after slicing :", Alphabets[0:2])
+print("The string after slicing :",Alphabets[0:2])
