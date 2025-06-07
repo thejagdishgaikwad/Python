635a2ec08fbaa96fe6f928bf90bfaab7c51e1ba4
Jagdish <jagdishgaikwad8765@gmail.com>
2025-06-07 21:06:00 +0530

    Write a python program to get 3 fav movies from user and store them into list

---
+++ Lecture2/PRACTICE/2A.py
@@ -0,0 +1,8 @@
+#Write a python program to ask user of their 3 Favorite Movies and store them in list []
+Mov1 = input("Enter First Fav Movie :")
+Mov2 = input("Enter Second Fav Movie :")
+Mov3 = input("Enter Third Fav Movie :")
+
+Movie_names = [Mov1, Mov2, Mov2]
+print("The Fav Movies Of Users Choice's Are ", Movie_names)
+print(type(Movie_names))
