942a89b690b90bbc6422d6cf043c473fcfe6e0ea
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2026-05-21 21:07:49 +0530

    need updation or like needs to be fixed

---
+++ May/Operators/excercise/five.py
@@ -0,0 +1,7 @@
+#If you run a 10 kilometer race in 42 minutes 42 seconds, what is your average pace (time per mile in minutes and seconds)? What is your average speed in miles per hour?
+
+mile = 10/1.6
+time = 42*60+42
+hour = time/60
+
+print("The avg time for miles per hour : ", mile/hour)
