96f52fff991f082374be932dfad7705ab839e2d2
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2026-05-25 18:01:34 +0530

    Word Problem

---
+++ May/Operators/excercise/book.py
@@ -0,0 +1,7 @@
+#Given : 
+# Book Price = 14.97, Shipping Cost = 3+47.25, and total price for 60 copies = ?
+
+Book_Price = 14.97*60
+ShippingCost = 3+(0.75*59)
+
+print("The Wholesale Price For 60 Copies : ", Book_Price+ShippingCost)
