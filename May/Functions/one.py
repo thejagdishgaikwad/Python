efec9dce7e8ef836ec4ef31d8c0046364343fe11
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2026-05-25 18:15:48 +0530

    test fn

---
+++ May/Functions/one.py
@@ -0,0 +1,7 @@
+def jagdish(age,bornyear):
+    print(f"My Name Is Jagdish Raosaheb Gaikwad!\n And my Age is {age} and i am born in year {bornyear}")
+
+age = int(input("Enter Age : "))
+bornyear = int(input("Born Year : "))
+
+jagdish(age,bornyear)
