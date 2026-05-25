063791a7123c2871df0b6fd860d105039a0cd5e9
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2026-05-25 18:10:02 +0530

    DEL

--- 1A.py
+++
@@ -1,4 +0,0 @@
-import datetime
-name=input("Enter your name:")
-age=int(input("Enter your age:"))
-print("You will become 100 years old in:",(100-age)+datetime.datetime.now().year)
--- 2C.py
+++
@@ -1,5 +0,0 @@
-def histogram(inList):
-    for i in range(len(inList)):
-        print(inList[i]*'*')
-List=[4,9,7]
-histogram(List)
