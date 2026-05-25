e0aafc27ea7c0f7e63b8b113904dc6aa09192b57
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2026-05-25 17:08:29 +0530

    Third Program

---
+++ May/third.py
@@ -0,0 +1,6 @@
+#The volume of a sphere with radius r is 4/3 pr3. What is the volume of a sphere with radius 5?
+
+radius = int(input("Enter Radius : "))
+pi = 3.14
+vol_sphere = 4/3*pi*radius**3
+print(f"Volume of sphere, where radius is {radius} and pi is {pi} : ", vol_sphere)
