5023b7110b4dc80415b452e4982f134d0c72264c
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2026-06-22 21:25:30 +0530

    SelfPractice

---
+++ May/Operators/excercise/Practise.py
@@ -0,0 +1,10 @@
+#implicit conversion
+number = int(input("Enter Integer : "))
+print(float(number))
+
+#explicit conversion
+Number = float(input("Enter Float Value : "))
+print(int(Number))
+
+Data = int(input("Enter Integer Value :\n"))
+print(type(str(Data)))
