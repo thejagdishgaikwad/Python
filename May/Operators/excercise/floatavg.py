04e3041bd90087437dfa6da7727433aad286d52e
thejagdishgaikwad <jagdishgaikwad8765@gmail.com>
2026-06-22 21:34:55 +0530

    Added float avg of 2 numbers

---
+++ May/Operators/excercise/floatavg.py
@@ -0,0 +1,4 @@
+no1 = float(input("Enter Float Number : "))
+no2 = float(input("Enter Float Number : "))
+
+print(f"Avg of {no1} and {no2} : ", (no1+no2)/2) #note! first i did this expn no1+no2/2, but i was wrong since python's interpreter follows another rules or set of programming. SO as per the precedence the right expn will be (no1+no2)/2
